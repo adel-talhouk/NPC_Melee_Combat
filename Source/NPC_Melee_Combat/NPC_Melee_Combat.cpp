b23d488973adfe73284f4b8458d5493780cbba37
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2025-02-01 18:37:45 -0800

    Temp animation, and added cooldown. UI being updated. Need to reset animation and then deal damage

---
+++ Source/NPC_Melee_Combat/NPC_Melee_Combat.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NPC_Melee_Combat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NPC_Melee_Combat, "NPC_Melee_Combat" );
---
+++ Source/NPC_Melee_Combat/Public/CombatEncounterInstanceManager.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class NPC_MELEE_COMBAT_API CombatEncounterInstanceManager
+{
+public:
+	CombatEncounterInstanceManager();
+	~CombatEncounterInstanceManager();
+};
